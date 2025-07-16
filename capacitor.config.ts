8aff6af87120708b162236f51899a98bae0abba5
AlydiaHZ <zrj55845@gmail.com>
2025-07-17 01:50:17 +0800

    feat(app): rename and change logo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,12 +2,12 @@ import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
   appId: 'com.down.paikexitong',
-  appName: 'paikexitong',
+  appName: '排课系统',
   webDir: 'dist',
-  server: {
-    androidScheme: 'http',
-    url: 'http://192.168.1.24:5173/',
-  },
+  // server: {
+  //   androidScheme: 'http',
+  //   url: 'http://192.168.1.24:5173/',
+  // },
 }
 
 export default config
