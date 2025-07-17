7b689006db441989bd9dd7a01cae61fe10fa45e0
AlydiaHZ <zrj55845@gmail.com>
2025-07-17 11:46:31 +0800

    feat: fix script

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,23 @@
 import type { CapacitorConfig } from '@capacitor/cli'
+import process from 'node:process'
+
+// 获取当前环境（development/production）
+const mode = process.env.NODE_ENV || 'production'
+
+const isDev = mode.trim() === 'development'
 
 const config: CapacitorConfig = {
   appId: 'com.down.paikexitong',
   appName: '排课系统',
   webDir: 'dist',
-  // server: {
-  //   androidScheme: 'http',
-  //   url: 'http://192.168.1.24:5173/',
-  // },
+  ...(isDev
+    ? {
+        server: {
+          androidScheme: 'http',
+          url: 'http://192.168.1.24:5173/',
+        },
+      }
+    : {}),
 }
 
 export default config
